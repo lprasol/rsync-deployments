267ab0a7788edc8f0213c00f5edc22c81e007c92
lprasol <lprasol@gmail.com>
2020-03-03 08:59:37 +0100

    tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,10 @@ echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
 md5sum $SSH_PATH/deploy_key
+echo "ENV"
+echo $1
+echo $2
+echo $3
+
 # Do deployment
 sh -c "rsync $1 -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $2 $GITHUB_WORKSPACE/ $3"
